4f35a45d79120bddff3eca9f215bc49170c92f8d
murat-aka <murat-aka@hotmail.com>
2015-10-24 18:57:42 +0100

    week2 quizFindWebLinks

---
+++ findWebLinks/WebLinks.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of WebLinks here.
+ * 
+ * Write a program that reads the lines from the 
+ * file at this URL location,
+ * http://www.dukelearntoprogram.com/course2/data/manylinks.html,
+ * and prints each URL on the page that is a link to youtube.com.
+ * 
+ * @author (murat aka) 
+ * @version (24/10/2015)
+ */
+public class WebLinks {
+
+    
+    
+}
