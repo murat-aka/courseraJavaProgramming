c2bb35b83bd0715df4760a7732593a78a8da6319
murat-aka <murat-aka@hotmail.com>
2015-11-08 18:00:18 +0000

    update testColdestHourInFile()

--- week3/part2/weatherCVSproblem/WeatherCVSproblem.java
+++ week3/part2/weatherCVSproblem/WeatherCVSproblem.java
@@ -15,66 +15,69 @@ import java.io.*;
 public class WeatherCVSproblem {
     
     
-   	public CSVRecord coldestHourInFile(CSVParser parser) {
-		//start with largestSoFar as nothing
-		CSVRecord smallestSoFar = null;
-		//For each row (currentRow) in the CSV File
-		for (CSVRecord currentRow : parser) {
-			// use method to compare two records
-			smallestSoFar = getSmallestOfTwo(currentRow, smallestSoFar);
-		}
-		//The largestSoFar is the answer
-		return smallestSoFar;
-	}
+    public CSVRecord coldestHourInFile(CSVParser parser) {
+        //start with largestSoFar as nothing
+        CSVRecord smallestSoFar = null;
+        //For each row (currentRow) in the CSV File
+        for (CSVRecord currentRow : parser) {
+            // use method to compare two records
+            smallestSoFar = getSmallestOfTwo(currentRow, smallestSoFar);
+        }
+        //The largestSoFar is the answer
+        return smallestSoFar;
+    }
     
-	
-	public void testColdestHourInFile(){
+    
+    public void testColdestHourInFile(){
+        
+        FileResource fr = new FileResource();
+		CSVRecord smallest = coldestHourInFile(fr.getCSVParser());
+        System.out.println("coldest temperature was " + smallest.get("TemperatureF") +
+                   " at " + smallest.get("DateUTC"));
+    }
+    
+        
+    public void testFileWithColdestTemperature(){
+        
 		CSVRecord smallest = fileWithColdestTemperature();
 		System.out.println("coldest temperature was " + smallest.get("TemperatureF") +
 				   " at " + smallest.get("DateUTC"));
 	}
 	
 	
-	public void testHottestInDay () {
-		FileResource fr = new FileResource("data/2015/weather-2015-01-01.csv");
-		CSVRecord largest = coldestHourInFile(fr.getCSVParser());
-		System.out.println("hottest temperature was " + largest.get("TemperatureF") +
-				   " at " + largest.get("TimeEST"));
-	}
+    public CSVRecord  fileWithColdestTemperature() {
+        CSVRecord coldestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        // iterate over files
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            // use method to get smallest in file.
+            CSVRecord currentRow = coldestHourInFile(fr.getCSVParser());
+            // use method to compare two records
+            coldestSoFar = getSmallestOfTwo(currentRow, coldestSoFar);
+        }
+        //The coldestSoFar is the answer
+        return coldestSoFar;
+    }
 
-	public CSVRecord  fileWithColdestTemperature() {
-		CSVRecord coldestSoFar = null;
-		DirectoryResource dr = new DirectoryResource();
-		// iterate over files
-		for (File f : dr.selectedFiles()) {
-			FileResource fr = new FileResource(f);
-			// use method to get smallest in file.
-			CSVRecord currentRow = coldestHourInFile(fr.getCSVParser());
-			// use method to compare two records
-			coldestSoFar = getSmallestOfTwo(currentRow, coldestSoFar);
-		}
-		//The coldestSoFar is the answer
-		return coldestSoFar;
-	}
+    public CSVRecord getSmallestOfTwo (CSVRecord currentRow, CSVRecord smallestSoFar) {
+        //If largestSoFar is nothing
+        if (smallestSoFar == null) {
+            smallestSoFar = currentRow;
+        }
+        //Otherwise
+        else {
+            double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+            double smallestTemp = Double.parseDouble(smallestSoFar.get("TemperatureF"));
+            //Check if currentRow’s temperature > largestSoFar’s
+            if (currentTemp < smallestTemp) {
+                //If so update largestSoFar to currentRow
+                smallestSoFar = currentRow;
+            }
+        }
+        return smallestSoFar;
+    }
 
-	public CSVRecord getSmallestOfTwo (CSVRecord currentRow, CSVRecord smallestSoFar) {
-		//If largestSoFar is nothing
-		if (smallestSoFar == null) {
-			smallestSoFar = currentRow;
-		}
-		//Otherwise
-		else {
-			double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
-			double smallestTemp = Double.parseDouble(smallestSoFar.get("TemperatureF"));
-			//Check if currentRow’s temperature > largestSoFar’s
-			if (currentTemp < smallestTemp) {
-				//If so update largestSoFar to currentRow
-				smallestSoFar = currentRow;
-			}
-		}
-		return smallestSoFar;
-	}
-
-	
+    
 
 }
